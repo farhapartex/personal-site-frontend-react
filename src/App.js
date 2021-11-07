389b944ee79e13a6bdaf5636b3b508e0cb6c9060
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-07 22:02:01 +0600

    nav menu added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import RootPage from "./pages/RootPage";
 import './App.css';
+import NavMenu from "./component/NavMenu";
 
 
 function App() {
   return (
     <div className="App">
+      <NavMenu/>
         <Routes>
           <Route exact path="/" element={<RootPage/>}></Route>
         </Routes>
---
+++ src/component/NavMenu.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+class NavMenu extends Component{
+    render(){
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Photography</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default NavMenu;
