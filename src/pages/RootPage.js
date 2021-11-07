f93cd47bafc4a4a8ddf014422ca4de2be6469f75
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-07 21:30:22 +0600

    root component added with react router

---
+++ src/pages/RootPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class RootPage extends Component{
+    render(){
+        console.log("Working");
+        return (
+            <div>
+                <p>This site under construction</p>
+            </div>
+        )
+    }
+}
+
+
+export default RootPage;
